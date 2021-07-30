21e1da3e492425f3df9c8f591b40929e9e9c04c6
Abhishek-st <ankit2632000@gmail.com>
2021-07-30 17:37:18 +0530

    added all the api

--- api/views.py
+++ api/views.py
@@ -14,5 +14,25 @@ class Countries(APIView):
 class CountriesFilter(APIView):
     def get(self, request, pk):
         print(pk)
-        print(request.GET)
-        return Response({'mag':'yes'})
+        data = request.GET
+        odata = Climate.objects.filter(country_or_area=pk)
+        if 'startYear' in data and 'endYear'in data and 'cat'in data:
+            print('3')
+            odata = Climate.objects.filter(country_or_area=pk, year__gte=int(data['startYear']), year__lte=int(data['endYear']), category=data['cat']) 
+        elif 'startYear' in data and 'endYear'in data:
+            print('2')
+            print(data['startYear'])
+            print(data['endYear'])
+            odata = Climate.objects.filter(country_or_area=pk, year__gte=int(data['startYear']), year__lte=int(data['endYear']))
+        elif 'startYear' in data and 'cat'in data:
+            print('1')
+            odata = Climate.objects.filter(country_or_area=pk, year__gte=int(data['startYear']), category=data['cat']) 
+        elif 'cat'in data:
+            print('0')
+            odata = Climate.objects.filter(country_or_area=pk, category=data['cat']) 
+        elif 'startYear'in data:
+            print('0')
+            odata = Climate.objects.filter(country_or_area=pk, year__gte=int(data['startYear'])) 
+
+        ser = CLimateSerializer(odata, many=True)
+        return Response(ser.data)
